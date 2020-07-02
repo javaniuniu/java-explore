8edf0b68e129d2ade0def3b94bba2fb6458857e7
javaniuniu <king101125s@gmail.com>
2020-07-02 23:37:45 +0800

    多线程进阶==>JUC编发编程
    多线程进阶==>JUC编发编程

---
+++ juc/src/main/java/javaniuniu/unsafe/MapTest.java
@@ -0,0 +1,24 @@
+package javaniuniu.unsafe;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+// java.util.ConcurrentModificationException
+public class MapTest {
+    public static void main(String[] args) {
+        /**
+         * 解决方案
+         * 1、Map<String,String> map = Collections.synchronizedMap(new HashMap<>());
+         * 2、Map<String,String> map = new ConcurrentHashMap<>();
+         */
+        Map<String, String> map = new ConcurrentHashMap<>();
+        for (int i = 0; i <10 ; i++) {
+            new Thread(()-> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0,5));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
---
+++ juc/src/main/java/javaniuniu/unsafe/SetTest.java
@@ -0,0 +1,26 @@
+package javaniuniu.unsafe;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+
+// java.util.ConcurrentModificationException
+public class SetTest {
+    public static void main(String[] args) {
+        /**
+         * 解决方案：
+         * 1、Set<String> list = Collections.synchronizedSet(new HashSet<>());
+         * 2、Set<String> list = new CopyOnWriteArraySet();
+         */
+
+//        Set<String> list = new CopyOnWriteArraySet();
+        Set<String> list = new HashSet<>();
+        for (int i = 0; i <20 ; i++) {
+            new Thread(()->{
+                list.add(UUID.randomUUID().toString().substring(5));
+                System.out.println(list);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
